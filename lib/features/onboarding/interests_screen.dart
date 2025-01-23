1c97d94425bc62fbc4956e92b4799de3bf1a4c33
generation21 <qbration21@gmail.com>
2025-01-24 01:22:04 +0900

    feat: add InterestsScreen for onboarding process, allowing users to select interests for personalized video recommendations; update main app to display InterestsScreen instead of SignUpScreen

--- lib/features/onboarding/interests_screen.dart
+++ lib/features/onboarding/interests_screen.dart
@@ -1,13 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/constants/sizes.dart';
+
+const interests = [
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+  "Daily Life",
+  "Comedy",
+  "Entertainment",
+  "Animals",
+  "Food",
+  "Beauty & Style",
+  "Drama",
+  "Learning",
+  "Talent",
+  "Sports",
+  "Auto",
+  "Family",
+  "Fitness & Health",
+  "DIY & Life Hacks",
+  "Arts & Crafts",
+  "Dance",
+  "Outdoors",
+  "Oddly Satisfying",
+  "Home & Garden",
+];
 
 class InterestsScreen extends StatelessWidget {
   const InterestsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Onboarding"),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Choose your interests")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: Sizes.size24,
+            right: Sizes.size24,
+            bottom: Sizes.size16,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v32,
+              const Text(
+                "Choose your interests",
+                style: TextStyle(
+                  fontSize: Sizes.size40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Gaps.v20,
+              const Text(
+                "Get better video recommendations",
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                ),
+              ),
+              Gaps.v16,
+              Wrap(
+                runSpacing: Sizes.size14,
+                spacing: Sizes.size14,
+                children: [
+                  for (var interest in interests)
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: Sizes.size16,
+                        horizontal: Sizes.size24,
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color:
+                                  Colors.black.withAlpha((255 * 0.05).toInt()),
+                              blurRadius: Sizes.size5,
+                              spreadRadius: Sizes.size5,
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(Sizes.size32),
+                          border: Border.all(color: Colors.black12)),
+                      child: Text(interest,
+                          style: const TextStyle(fontWeight: FontWeight.w600)),
+                    ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 0,
+              blurRadius: 10,
+              offset: const Offset(0, -1),
+            ),
+          ],
+        ),
+        child: BottomAppBar(
+          color: Colors.white,
+          surfaceTintColor: Colors.transparent,
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(Sizes.size4),
+            ),
+            child: const Text(
+              "Next",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok/constants/sizes.dart';
 import 'package:tiktok/features/authentication/sign_up_screen.dart';
+import 'package:tiktok/features/onboarding/interests_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -14,20 +15,22 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFFE0435A),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
-            color: Colors.black,
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: const Color(0xFFE0435A),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
           ),
-        ),
-      ),
+          bottomAppBarTheme: const BottomAppBarTheme(
+            color: Colors.white,
+          )),
       home: const Scaffold(
-        body: SignUpScreen(),
+        body: InterestsScreen(),
       ),
     );
   }
