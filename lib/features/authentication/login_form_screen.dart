68c4fe74f7eec6d899f62be8993af7ae70c21975
generation21 <qbration21@gmail.com>
2025-01-24 01:22:20 +0900

    feat: integrate navigation to InterestsScreen in LoginFormScreen after form submission, enhancing user flow in authentication process

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 import 'package:tiktok/features/authentication/widgets/form_button.dart';
+import 'package:tiktok/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -19,6 +20,12 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     if (_formKey.currentState == null) return;
     if (!_formKey.currentState!.validate()) return;
     _formKey.currentState!.save();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const InterestsScreen(),
+      ),
+    );
   }
 
   @override
